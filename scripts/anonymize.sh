52820b4ecf41425495ed10b11b1b9860c71c6300
jeroneandrews <jerone.andrews.13@ucl.ac.uk>
2020-10-08 17:53:46 +0100

    updated comments

--- scripts/anonymize.sh
+++ scripts/anonymize.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env bash
 
-# evaluator for Cama
+# train Cama
 python train.py --dis_input con_conv \
 --clf_low_reload /models/classifiers/adversary/resnet18/prnu_lp_low/train/**/best.pth \
 --clf_high_reload /models/classifiers/adversary/resnet18/prnu_lp/train/**/best.pth \
 --est_prnu_reload /models/estimators/adversary/prnu_lp/train/**/best_rmse.pth
 
-# evaluator for SpoC
+# train SpoC
 python train.py--dis_input rgb+finite_difference \
 --adv_loss_schedule 0.3 0 --clf_low_loss_schedule 0.0 0 --clf_high_loss_schedule 0.01 0 \
 --clf_high_reload /models/classifiers/adversary/resnet18/rgb+finite_difference/train/**/best.pth
 
-# evaluator for MISL
+# train MISL
 python train.py --dis_input con_conv --clf_low_loss_schedule 0.0 0 --clf_high_loss_schedule 0.01 0 \
 --clf_noise_reload/models/classifiers/adversary/resnet18/con_conv/train/**/best.pth
 
